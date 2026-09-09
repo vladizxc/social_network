0caeb29106a064f220f5a7f16c9cf1f98755b8b8
vladizxc <vlad08052@gmx.de>
2026-09-09 17:55:11 +0200

    first entities

---
+++ src/main/java/com/social_network/practice/entity/Post.java
@@ -0,0 +1,34 @@
+package com.social_network.practice.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Post {
+
+    private long post_id;
+
+    private String content;
+    private long user_id;
+
+    public Post(){}
+
+    public Post(String content){
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ src/main/java/com/social_network/practice/entity/User.java
@@ -0,0 +1,51 @@
+package com.social_network.practice.entity;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class User {
+
+    private long id;
+
+    private String name;
+    private ArrayList<Post> posts;
+    private ArrayList<Comment> comments;
+
+    public User() {}
+
+    public User(String name) {
+        this.name = name;
+        this.posts = new ArrayList<>();
+        this.comments = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return comments;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosts(ArrayList<Post> posts) {
+        this.posts = posts;
+    }
+
+    public void setComments(ArrayList<Comment> comments) {
+        this.comments = comments;
+    }
+}
