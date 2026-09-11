d8da2ae3f5d4dc325aa9f9ed8052099c558771dd
vladizxc <vlad08052@gmx.de>
2026-09-11 13:37:48 +0200

    repositories added

---
+++ src/main/java/com/social_network/practice/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.social_network.practice.repository;
+
+import com.social_network.practice.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
